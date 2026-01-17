46c6769bcfdd506cbc1409bb10fc9318e93b6aa5
Juan Darley <juandarley99@gmail.com>
2026-01-17 20:14:12 -0300

    add column telefone table medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,16 @@
 package med.voll.api.controller;
 
-import med.voll.api.endereco.Endereco;
+
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -19,8 +21,15 @@ public class MedicoController {
 
 
     @PostMapping
-    public void cadastrar (@RequestBody DadosCadastroMedico dados)
+    @Transactional
+    public void cadastrar (@RequestBody @Valid DadosCadastroMedico dados)
     {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar (){
+
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
